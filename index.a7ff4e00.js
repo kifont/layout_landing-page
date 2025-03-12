e7dc15623efe40acae81632e411d64da48dbe69f
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-03-12 10:45:15 +0200

    make build

---
+++ index.a7ff4e00.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("contactForm").addEventListener("submit",function(t){t.preventDefault(),alert("Форма отправлена!"),this.reset()})});
+//# sourceMappingURL=index.a7ff4e00.js.map
